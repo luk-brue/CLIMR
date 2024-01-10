481dd81821c0656b536011c59a4660a87e5d789d
Lukas Bruelheide <zur@posteo.de>
2024-01-11 00:34:12 +0100

    DE 01: Add deviation handlings
    - expand test case removal
    - remove some manually noted IDs
    - clean up the id_subject column so it follows a consistent naming scheme to discern two locations

--- R/import/CLIMR_deviation-handling.R
+++ R/import/CLIMR_deviation-handling.R
@@ -80,13 +80,105 @@ raw <- raw %>%
 
 # DE_01 tested the experiments a few times and identified these cases by
 # entering "climr" or "climbr" in the ID field or the ethnicity field. These
-# cases should be removed.
+# cases should be removed. There are also some other random character strings
+# in the id field that were used to mark a test case. 
 
-raw <- raw %>% 
-  filter(id_subject != "climr"  | is.na(id_subject)) %>% 
-  filter(id_subject != "climbr" | is.na(id_subject)) %>% 
+# First step, get all IDs that contain non-numeric characters. 
+
+de01_non_num <- raw %>% 
+  filter(lab == "DE_01") %>% 
+  purrr::pluck("id_subject") %>% 
+  stringi::stri_subset(regex = "[^0-9]")
+
+# Second: Not all of these are test cases. IDs that end with "CC" or "cc"
+# should be kept, they are valid. 
+# (The CC suffix arose when experimenters started
+# collecting data in a second location called CC, and did not yet have a
+# standard for giving discernable names to IDs from this location. Later on, a 
+# standard was established: every ID that is > 500 comes from the second
+# location)
+
+de01_testcases <- de01_non_num[
+  !(endsWith(de01_non_num, "CC") | endsWith(de01_non_num, "cc"))] |>
+  unique()
+
+# Third: Remove the test cases, also remove a test case that was not covered
+# by the non-numeric approach (ID 00000). 
+
+raw <- raw %>%
+  filter(!is.na(id_subject)) %>% 
+  filter(!(lab == "DE_01" & id_subject %in% de01_testcases)) %>% 
+  filter(!(lab == "DE_01" & id_subject == "00000")) %>% 
   filter(ethnicity  != "climr"  | is.na(ethnicity))
 
+# DE_01: Invalid or incomplete cases -------------------------------------------
+
+# DE_01 noted some subject IDs which are invalid and should be excluded.
+
+# Person with ID 609 did not read an instruction and could not go back
+# to re-read it. Experimenters started a fresh questionnaire with a new ID,
+# 610, which was completed. Therefore 609 is an incomplete duplicate.
+
+raw <- raw %>% 
+  filter(!(lab == "DE_01" & id_subject == 609))
+
+# On two occasions (IDs 71 and 754), the computer with the questionnaire
+# crashed, and experimenters gave participants a fresh questionnaire but entered
+# the same subject_id as in the crashed questionnaire. A quick way to decide 
+# which of these duplicates is the incomplete case: Look at the the panas_1 
+# column, which is near the end of the questionnaire, to discern incomplete from
+# complete duplicate
+
+raw <- raw %>% 
+  filter(!(lab == "DE_01" & id_subject %in% c(71, 754) & is.na(panas_1) == TRUE))
+
+# Person 713 did not find their national identity in the drop down list.
+# They entered Germany because the field is required. But now, afterwards it 
+# should be set to NA to reflect reality that national identity of that person 
+# is unknown. 
+
+raw$national_identity[raw$id_subject == 713 & raw$lab == "DE_01"] <- NA
+
+# DE_01: Clean Up IDs from different locations----------------------------------
+
+# DE_01 used two different locations for data collection.
+# id_subject was used to discern those, but for the first few persons at the
+# second location, there is no consistent scheme. Later, a consistent scheme was
+# adopted - an ID > 500 means the data were collected at the second location.
+# The IDs of the first few persons that do not adhere to this should be
+# retrofitted with the > 500 scheme. This facilitates location-wise analysis.
+
+# The IDs 1 to 5 which were recorded on 2023-12-11 should be renamed 501 to 505:
+# Best way for this might be to use `sub` which uniquely identifies each ID.
+# This assumes `sub` is unique even across all labs
+
+first_five <- raw %>% 
+  filter(lab == "DE_01") %>% 
+  filter(as_date(recorded_date) == as_date("2023-12-11")) %>% 
+  filter(id_subject %in% 1:5) %>% 
+  select(recorded_date, id_subject, sub)
+
+raw$id_subject[raw$sub %in% first_five$sub] <- 
+  as.numeric(first_five$id_subject) + 500
+
+# The IDs 7 to 15 have been suffixed with either "CC" or "cc".
+# Get subs of those IDs
+cc_suffix <- raw %>% 
+  filter(lab == "DE_01") %>% 
+  filter(id_subject %>%  endsWith("CC") |
+           id_subject %>% endsWith("cc")) %>% 
+  select(sub, id_subject)
+
+# Remove the last two characters from those IDs and add 500
+
+raw$id_subject[raw$lab == "DE_01" & raw$sub %in% cc_suffix$sub] <-
+  substr(cc_suffix$id_subject, 1, nchar(cc_suffix$id_subject) - 2) %>% 
+    as.numeric() %>% 
+    `+`(500)
+
+# Clean up...
+rm(cc_suffix, de01_non_num, de01_testcases)
+  
 # Duplicated participant IDs ---------------------------------------------------
 
 # Some labs reported accidentally reusing some participant ID values These
